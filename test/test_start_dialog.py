6858a3ffdeae7b1fbf74064e6ca85146ce8f116f
Marin Manuel <marin.manuel@neurobio.org>
2022-01-12 22:06:38 -0500

    pygame works at this point.

---
+++ test/test_start_dialog.py
@@ -0,0 +1,13 @@
+import sys
+import json
+from PyQt5.QtWidgets import QApplication
+from GUI.GUI import StartDialog
+
+sys.path.insert(0, "./GUI")
+
+with open('./PhysioMonitor.json', 'r', encoding='utf-8') as f:
+    config = json.load(f)
+
+app = QApplication(sys.argv)
+dlg = StartDialog(config=config)
+dlg.exec()
