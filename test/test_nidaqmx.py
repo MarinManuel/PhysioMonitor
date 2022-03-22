79785a7dbe36406e661bc9e43ae34260ee546861
Marin Manuel <marin.manuel@neurobio.org>
2022-03-22 15:34:14 -0400

    adjusted temp scaling and pump baudrate

--- test/test_nidaqmx.py
+++ test/test_nidaqmx.py
@@ -1,7 +1,12 @@
+import pprint
 import nidaqmx
-with nidaqmx.Task() as task:
-    task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
-    out = task.read()
+import nidaqmx.constants
 
-print(out)
-type(out)
+pp = pprint.PrettyPrinter(indent=4)
+
+
+with nidaqmx.Task() as task:
+    task.ai_channels.add_ai_voltage_chan("Dev1/ai0:7",
+                                         terminal_config=nidaqmx.constants.TerminalConfiguration.BAL_DIFF)
+    data = task.read(number_of_samples_per_channel=1)
+    print(data)
